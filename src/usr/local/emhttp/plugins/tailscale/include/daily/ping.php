3e6cb1cfe509871ec1503e0ec0c99b57c37e2cca
Derek Kaser <derek.kaser@gmail.com>
2024-01-28 23:55:06 -0500

    feat(usage): improve error handling
    Signed-off-by: Derek Kaser <derek.kaser@gmail.com>

--- src/usr/local/emhttp/plugins/tailscale/include/daily/ping.php
+++ src/usr/local/emhttp/plugins/tailscale/include/daily/ping.php
@@ -2,6 +2,17 @@
 
 $endpoint = "https://plugin-usage.edacerton.win/";
 
+function send_usage($url) {
+    $c = curl_init();
+    curl_setopt($c, CURLOPT_URL, $url);
+    curl_exec($c);
+    if (!curl_errno($c)) {
+        $info = curl_getinfo($c);
+        return $info['http_code'];
+    }
+    return -1;
+}
+
 if ($tailscale_config['USAGE']) {
     if ( ! isset($var)) {
         $var = parse_ini_file('/usr/local/emhttp/state/var.ini');
@@ -12,18 +23,22 @@ if ($tailscale_config['USAGE']) {
         'clientId' => hash("crc32b", $var['flashGUID']),
         'plugin'   => 'tailscale',
         'version'  => $version['VERSION'],
+        'branch'    => $version['BRANCH'],
         'unraid'   => $var['version']
     );
 
     $queryString = http_build_query($query);
 
-    sleep(rand(0, 60));
-
     logmsg("Sending usage data: {$queryString}");
+    $attempts = 0;
+    do {
+        sleep(rand(0, 600));
 
-    $request = file_get_contents("{$endpoint}?{$queryString}");
+        $attempts++;
+        $result = send_usage("{$endpoint}?{$queryString}");
+    } while (($result != '201') && ($attempts < 5));
 
-    if ( ! mb_strpos($http_response_header[0], '201')) {
+    if ($result != '201') {
         logmsg("Error occurred while transmitting usage data.");
     }
 } else {
