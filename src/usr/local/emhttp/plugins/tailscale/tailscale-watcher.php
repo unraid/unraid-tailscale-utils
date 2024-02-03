7a406a07da97ba04c134ba3b8450c6bceee61d63
Derek Kaser <derek.kaser@gmail.com>
2024-02-03 13:06:48 -0500

    feat(tailscale-watcher): add check to see if nginx reloaded correctly
    Signed-off-by: Derek Kaser <derek.kaser@gmail.com>

--- src/usr/local/emhttp/plugins/tailscale/tailscale-watcher.php
+++ src/usr/local/emhttp/plugins/tailscale/tailscale-watcher.php
@@ -20,6 +20,9 @@ while (true) {
             if (isset($interface["address"])) {
                 $new_addresses[] = $interface["address"];
                 $timer           = 60;
+                if($interface["family"] == 2) { 
+                    $tailscale_ipv4 = $interface["address"];
+                }
             }
         }
     }
