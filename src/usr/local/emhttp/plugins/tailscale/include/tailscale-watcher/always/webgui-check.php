707334840d558f2ef7993e608dc06618bc7b9ea2
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2024-09-30 02:06:13 +0000

    fix: incorrect restart command for nginx check

--- src/usr/local/emhttp/plugins/tailscale/include/tailscale-watcher/always/webgui-check.php
+++ src/usr/local/emhttp/plugins/tailscale/include/tailscale-watcher/always/webgui-check.php
@@ -10,8 +10,8 @@ if ($tailscale_config["INCLUDE_INTERFACE"] == 1) {
         logmsg("WebGUI listening on {$tailscale_ipv4}:{$ident_config['PORT']}");
     } else {
         logmsg("WebGUI not listening on {$tailscale_ipv4}:{$ident_config['PORT']}, terminating and restarting");
-        exec("/etc/init.d/rc.nginx term");
+        run_command("/etc/rc.d/rc.nginx term", true);
         sleep(5);
-        exec("/etc/init.d/rc.nginx start");
+        run_command("/etc/rc.d/rc.nginx start", true);
     }
 }
