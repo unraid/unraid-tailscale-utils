0fc3e51c0052c7fd949801903a87e1186bd21f62
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2024-11-04 22:25:51 -0500

    fix: cleanup tags on dashboard (#21)

--- src/usr/local/emhttp/plugins/tailscale/include/Pages/Dashboard.php
+++ src/usr/local/emhttp/plugins/tailscale/include/Pages/Dashboard.php
@@ -17,13 +17,13 @@ if ($tailscaleConfig->Enable) {
     $tailscale_dashboard = Utils::printDash($tr->tr("info.online"), $tailscaleDashInfo->Online);
     $tailscale_dashboard .= Utils::printDash($tr->tr("info.hostname"), $tailscaleDashInfo->HostName);
     $tailscale_dashboard .= Utils::printDash($tr->tr("info.dns"), $tailscaleDashInfo->DNSName);
-    $tailscale_dashboard .= Utils::printDash($tr->tr("info.ip"), implode("<br /><span class='w26'>&nbsp;</span>", $tailscaleDashInfo->TailscaleIPs));
+    $tailscale_dashboard .= Utils::printDash($tr->tr("info.ip"), implode("<br><span class='w26'>&nbsp;</span>", $tailscaleDashInfo->TailscaleIPs));
 }
 
 echo <<<EOT
     <tbody title="Tailscale">
     <tr><td>
-    <img style="margin-right: 8px; width: 32px; height: 32px" src="/plugins/tailscale/tailscale.png" /><div class='section'>Tailscale<br><span id='tailscale-temp'></span><br></div>
+    <img style="margin-right: 8px; width: 32px; height: 32px" src="/plugins/tailscale/tailscale.png" alt="Tailscale"><div class='section'>Tailscale<br><span id='tailscale-temp'></span><br></div>
     <a href="/Settings/Tailscale" title="_(Settings)_"><i class="fa fa-fw fa-cog control"></i></a>
     </td></tr>
     {$tailscale_dashboard}
