d76672202e88702e8e090dfcca235e86c87cab39
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2024-11-05 22:19:23 +0000

    fix: allow more pings

--- src/usr/local/emhttp/plugins/tailscale/include/data/Status.php
+++ src/usr/local/emhttp/plugins/tailscale/include/data/Status.php
@@ -86,7 +86,8 @@ switch ($_POST['action']) {
 
         foreach ($tailscaleInfo->getPeerStatus() as $peer) {
             if ($peer->Name == $_POST['host']) {
-                $out = implode("<br>", Utils::run_command("tailscale ping --c 3 {$peer->IP[0]}"));
+                $peerIP = escapeshellarg($peer->IP[0]);
+                $out = implode("<br>", Utils::run_command("tailscale ping {$peerIP}"));
                 break;
             }
         }
