5a67e88b9405555a150de3a47281b64bc0a2f50e
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2024-11-05 17:34:39 -0500

    feat: add filtering to lock page (#24)

--- src/usr/local/emhttp/plugins/tailscale/include/tailscale-lock/signing.php
+++ src/usr/local/emhttp/plugins/tailscale/include/tailscale-lock/signing.php
@@ -15,15 +15,45 @@ $tr = $tr ?? new Tailscale\Translator();
         return;
     }
 ?>
+
 <form markdown="1" method="POST" action="/update.php" target="progressFrame">
 <input type="hidden" name="#command" value="/usr/local/emhttp/plugins/tailscale/approve-nodes.php" />
-<table style="margin-top: 5px;">
-<?php
-foreach ($tailscaleInfo->getTailscaleLockPending() as $lockHost => $lockKey) {
-    echo "<tr><td><input type='checkbox' name='#arg[]' value='{$lockKey}' /></td><td>{$lockHost}<br />{$lockKey}</td></tr>";
-}
+<table class="unraid t2 tablesorter" id="t2">
+    <thead>
+        <tr>
+            <th class="filter-false">&nbsp;</th>
+            <th>Name</th>
+            <th class="filter-false">Key</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($tailscaleInfo->getTailscaleLockPending() as $lockHost => $lockKey) {
+            echo "<tr><td><input type='checkbox' name='#arg[]' value='{$lockKey}' /></td><td>{$lockHost}</td><td>{$lockKey}</td></tr>";
+        }
 ?>
+    </tbody>
 </table>
 
 <input type="submit" name="#apply" value="<?= $tr->tr('Sign'); ?>">
 </form>
+<script>
+    function showLocks() {
+    $('#t2').tablesorter({
+      widthFixed : true,
+      sortList: [[0,0]],
+      sortAppend: [[0,0]],
+      widgets: ['stickyHeaders','filter','zebra'],
+      widgetOptions: {
+        // on black and white, offset is height of #menu
+        // on azure and gray, offset is height of #header
+        stickyHeaders_offset: ($('#menu').height() < 50) ? $('#menu').height() : $('#header').height(),
+        filter_columnFilters: true,
+        filter_liveSearch: true,
+
+        zebra: ["normal-row","alt-row"]
+      }
+    });
+}
+showLocks();
+</script>
