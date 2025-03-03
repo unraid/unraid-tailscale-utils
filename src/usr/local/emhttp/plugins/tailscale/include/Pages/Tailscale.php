48370026f548e0519f7293ccafb1429385c66c73
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-03-03 22:32:19 +0000

    feat: make new UI primary

--- src/usr/local/emhttp/plugins/tailscale/include/Pages/Tailscale.php
+++ src/usr/local/emhttp/plugins/tailscale/include/Pages/Tailscale.php
@@ -2,6 +2,9 @@
 
 namespace Tailscale;
 
+$docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
+require_once "{$docroot}/plugins/tailscale/include/common.php";
+
 $tailscaleConfig = $tailscaleConfig ?? new Config();
 $tr              = $tr              ?? new Translator();
 
