001e4ead1404c8bfacf6fea2daf3ecbff9c28831
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-05-20 00:27:50 -0400

    fix: ignore blank entries in locales (#43)

--- src/usr/local/emhttp/plugins/tailscale/include/Tailscale/Translator.php
+++ src/usr/local/emhttp/plugins/tailscale/include/Tailscale/Translator.php
@@ -7,6 +7,25 @@ class Translator
     /** @var array<string, string> $lang */
     private array $lang;
 
+    /**
+     * @return array<string, string>
+     * @param array<mixed,mixed> $array
+     */
+    private static function flattenArray(array $array, string $keys = ""): array
+    {
+        $result = array();
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                $result = array_merge($result, self::flattenArray($value, $keys . $key . "."));
+            } else {
+                if (is_string($value) && ! empty($value)) {
+                    $result[$keys . $key] = $value;
+                }
+            }
+        }
+        return $result;
+    }
+
     public function __construct()
     {
         global $login_locale;
@@ -19,25 +38,15 @@ class Translator
 
         $locale        = $_SESSION['locale'] ?? ($login_locale ?? ($dynamix['display']['locale'] ?? "none"));
         $plugin_locale = (array) json_decode(file_get_contents(PLUGIN_ROOT . "/locales/en_US.json") ?: "{}", true);
+        $plugin_lang   = self::flattenArray($plugin_locale);
 
         if (file_exists(PLUGIN_ROOT . "/locales/{$locale}.json")) {
             $current_locale = (array) json_decode(file_get_contents(PLUGIN_ROOT . "/locales/{$locale}.json") ?: "{}", true);
-            $plugin_locale  = array_replace_recursive($plugin_locale, $current_locale);
+            $current_lang   = self::flattenArray($current_locale);
+            $plugin_lang    = array_replace($plugin_lang, $current_lang);
         }
 
-        $ritit = new \RecursiveIteratorIterator(new \RecursiveArrayIterator($plugin_locale));
-        $lang  = array();
-        foreach ($ritit as $leafValue) {
-            $keys = array();
-            foreach (range(0, $ritit->getDepth()) as $depth) {
-                $keys[] = $ritit->getSubIterator($depth)->key();
-            }
-            if (is_string($leafValue)) {
-                $lang[ strtolower(join('.', $keys)) ] = $leafValue;
-            }
-        }
-
-        $this->lang = $lang;
+        $this->lang = $plugin_lang;
     }
 
     public function tr(string $message): string
